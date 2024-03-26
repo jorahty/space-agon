2877647ec314ce1699620433d1b204af2796501c
James Tennant <jorahty@gmail.com>
2024-03-26 16:15:49 -0700

    serve HTML

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+const port = 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
