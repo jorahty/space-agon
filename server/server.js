0d9897bd985af6dd69a757f95c70624f9217f79e
James Tennant <jorahty@gmail.com>
2024-03-26 16:30:23 -0700

    implement realtime chat with ws

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
-import express from 'express';
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
 const app = express();
-
 app.use(express.static('public'));
 
-const port = 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
+const port = 6969;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+server.listen(port, function () {
+  console.log(`Server is listening on ${port}!`);
+});
